fb11e3179a450f01541ee6357df04da1476a8fba
Nicholas Devenish <ndevenish@gmail.com>
2017-09-08 10:19:02 +0100

    Basic package template

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+setup(
+    name='tbx2cmake',
+    packages=[],
+    version='0.1.0',
+    description='Converts a TBX SCons-based build to CMake',
+)
