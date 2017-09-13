879453b89c9cb50a0195078b4883fd42877baab9
Nicholas Devenish <ndevenish@gmail.com>
2017-09-13 21:08:21 +0100

    Add missing package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tbx2cmake',
-    packages=[],
+    packages=["tbx2cmake"],
     version='0.1.0',
     description='Converts a TBX SCons-based build to CMake',
     entry_points = {
